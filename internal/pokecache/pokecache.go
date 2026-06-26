64757a9a7c08acd20b0743e3f15f8585c587df8e
PineappleOrca <kevinksingh05@gmail.com>
2026-06-26 14:40:16 +0100

    added more updates to the caching code

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -21,13 +21,39 @@ func NewCache(duration time.Duration) Cache {
 		cache:    make(map[string]cacheEntry),
 		interval: duration,
 	}
+	go newCache.reapLoop(duration)
+
 	return *newCache
 }
 
-func (c *Cache) Add(key string, val []byte) int {
-	return 0
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.cache[key] = cacheEntry{createdAt: time.Now(), val: val}
 }
 
-func (c *Cache) Get() ([]byte, bool) {
-	return make([]byte, 0), false
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	data, ok := c.cache[key]
+	if !ok {
+		return nil, false
+	}
+	container := make([]byte, 0)
+	container = append(container, data.val)
+	return container, true
+}
+
+func (c *Cache) reapLoop(duration time.Duration) {
+	ticker := time.NewTicker(duration)
+	for range ticker.C {
+		c.mu.Lock()
+		for key := range c.cache {
+			if time.Since(c.cache[key].createdAt) > duration {
+				delete(c.cache, key)
+			}
+		}
+		defer c.mu.Unlock()
+	}
+	defer ticker.Stop()
 }
